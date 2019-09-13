1bd081b02d874613b95fe891f2efbc8b272622be
Nathan Hannon <nathan@hannon.me>
2019-09-13 16:43:33 -0400

    Added Code to Main Activity

--- app/src/main/java/com/nathan/tictactoe/MainActivity.java
+++ app/src/main/java/com/nathan/tictactoe/MainActivity.java
@@ -1,15 +1,41 @@
 package com.nathan.tictactoe;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import static com.nathan.tictactoe.R.id.btnMidRight;
+import static com.nathan.tictactoe.R.id.btnNewGame;
+import static com.nathan.tictactoe.R.id.content;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View screen;
+    //buttons
+    private Button btnTopLeft;
+    private Button btnTopMiddle;
+    private Button btnTopRight;
+    private Button btnMidLeft;
+    private Button btnMiddle;
+    private Button btnMidRight;
+    private Button btnBottomLeft;
+    private Button btnBottomMiddle;
+    private Button btnBottomRight;
+
+    private Button btnNewGame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +43,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //TODO: Link CLick Events to Buttons
+        //TODO: Implement Board Reset Function
+
+        //Screen
+        screen = findViewById(content);
+        //screen.setOnClickListener(this);
+        //Top Row
+        btnTopLeft = findViewById(R.id.btnTopLeft);
+        btnTopMiddle = findViewById(R.id.btnTopMiddle);
+        btnTopRight = findViewById(R.id.btnTopRight);
+        //Middle Row
+        btnMidLeft = findViewById(R.id.btnMidLeft);
+        btnMiddle = findViewById(R.id.btnMiddle);
+        btnMidRight = findViewById(R.id.btnMidRight);
+        //Bottom Row
+        btnBottomLeft = findViewById(R.id.btnBottomLeft);
+        btnBottomMiddle = findViewById(R.id.btnBottomMiddle);
+        btnBottomRight = findViewById(R.id.btnBottomRight);
+        //New Game
+        btnNewGame = findViewById(R.id.btnNewGame);
+        //btnNewGame.setOnClickListener(this);
+        ResetBoard();
     }
 
     @Override
@@ -39,5 +88,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void ResetBoard(){
+        //Top Row
+        btnTopLeft.setText(R.string.btnBlank);
+        btnTopMiddle.setText(R.string.btnBlank);
+        btnTopRight.setText(R.string.btnBlank);
+        //Middle Row
+        btnMidLeft.setText(R.string.btnBlank);
+        btnMiddle.setText(R.string.btnBlank);
+        btnMidRight.setText(R.string.btnBlank);
+        //Bottom Row
+        btnBottomLeft.setText(R.string.btnBlank);
+        btnBottomMiddle.setText(R.string.btnBlank);
+        btnBottomRight.setText(R.string.btnBlank);
     }
 }
